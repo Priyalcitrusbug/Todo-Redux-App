eef9c3e34fcdd349ac18e420f798d7cfae6c6b91
Priyalcitrusbug <priyal.citrusbug@gmail.com>
2020-10-01 23:30:38 +0530

    Updates

---
+++ precache-manifest.1d5b1f88f641039827ae08f1699c07a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d7aabac0a7a2a21441627824c16c2f",
+    "url": "/Todo-Redux-App/index.html"
+  },
+  {
+    "revision": "6067f356110e3cfad843",
+    "url": "/Todo-Redux-App/static/js/2.71dd90a9.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Todo-Redux-App/static/js/2.71dd90a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd5e675fdb4f7c5de97",
+    "url": "/Todo-Redux-App/static/js/main.f50fbfbb.chunk.js"
+  },
+  {
+    "revision": "71b7d5d727a65c0a713d",
+    "url": "/Todo-Redux-App/static/js/runtime-main.61e00004.js"
+  }
+]);
